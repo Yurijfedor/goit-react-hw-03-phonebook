24f2e59f5646b31de117637009dd7ccc930f5e53
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-10-18 18:23:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { ContactList } from './phonebook/contactList';
 import { Filter } from './phonebook/filter';
 
 const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
 };
 const LS_KEY = 'my contacts';
@@ -19,10 +14,17 @@ const LS_KEY = 'my contacts';
 export class App extends Component {
   state = { ...INITIAL_STATE };
 
-  componentDidUpdate(prevProps, prevState) {
-    // if (this.state.contacts !== prevState.contacts) {
-    localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
-    // }
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(LS_KEY);
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state !== prevState) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
   }
 
   handleChange = evt => {
