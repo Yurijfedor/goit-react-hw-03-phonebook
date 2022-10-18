41217c1b58d8eba2b0bd08287178433eba3a7caa
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-10-18 17:59:12 +0300

    update package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,24 @@ import { ContactList } from './phonebook/contactList';
 import { Filter } from './phonebook/filter';
 
 const INITIAL_STATE = {
-  contacts: [],
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
   filter: '',
 };
+const LS_KEY = 'my contacts';
 
 export class App extends Component {
   state = { ...INITIAL_STATE };
+
+  componentDidUpdate(prevProps, prevState) {
+    // if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    // }
+  }
 
   handleChange = evt => {
     const name = evt.target.name;
